16f565cc683d13d9ba6f19b8afc110a8223186a9
Shubham <shubham16290@gmail.com>
2025-08-07 00:42:53 +0530

    rate-limiter-sliding-window

---
+++ rateLimiter/main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+type RateLimiter interface{
+	Allow(key string) bool
+}
+
+type SlidingWindowRateLimiter struct{
+	requests map[string][]time.Time // key = user / IP , value = list of timestamps
+	limit int
+	window time.Duration
+	mu sync.Mutex
+}
+
+func NewSlidingWindowRateLimiter(limit int, window time.Duration) *SlidingWindowRateLimiter {
+	return &SlidingWindowRateLimiter{
+		requests: make(map[string][]time.Time),
+		limit: limit,
+		window: window,
+	}
+}
+
+func (rl *SlidingWindowRateLimiter) Allow(key string) bool {
+	rl.mu.Lock()
+	defer rl.mu.Unlock()
+
+	now := time.Now()
+	windowStart := now.Add(-rl.window) // eg. taking last 10 second
+
+	// filter out timestamp outside the window
+	reqs := rl.requests[key]
+	var filtered []time.Time
+	for _ , t := range reqs {
+		if t.After(windowStart){
+			filtered = append(filtered, t)
+		}
+	}
+
+	// check if within limit
+	if len(filtered) >= rl.limit{
+		return false
+	}
+
+	filtered = append(filtered, now)
+	rl.requests[key] = filtered
+	return true
+}
+
+func main(){
+	rl := NewSlidingWindowRateLimiter(5,10*time.Second)
+
+	key := "user1"
+	for i := 0 ; i < 10 ; i++ {
+		allowed := rl.Allow(key)
+		fmt.Printf("Request %d allowed:%v\n" ,i+1 , allowed)
+		time.Sleep(1*time.Second)
+	}
+}
